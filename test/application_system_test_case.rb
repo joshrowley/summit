75ae98dd273fe6f9408a992c84cc8da50feaf031
Josh Rowley <joshua.f.rowley@gmail.com>
2025-04-27 00:01:19 -0400

    fix ci errors for selenium tests

--- test/application_system_test_case.rb
+++ test/application_system_test_case.rb
@@ -2,5 +2,12 @@ require "test_helper"
 
 class ApplicationSystemTestCase < ActionDispatch::SystemTestCase
   include Devise::Test::IntegrationHelpers
-  driven_by :selenium, using: :chrome, screen_size: [1400, 1400]
+
+  if ENV["CI"]
+    driven_by :selenium, using: :headless_chrome, screen_size: [1400, 1400], options: {
+      args: ["--no-sandbox", "--disable-dev-shm-usage", "--user-data-dir=#{Dir.tmpdir}/chrome_#{Process.pid}"]
+    }
+  else
+    driven_by :selenium, using: :chrome, screen_size: [1400, 1400]
+  end
 end
